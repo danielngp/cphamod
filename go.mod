4c363b024199fc9aa48ce01c06f8cb3843404b2d
Daniel Pinheiro <daniel.ngp@gmail.com>
2024-10-07 23:19:24 -0300

    1st commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Documents/code/sshClient
+
+go 1.22.2
+
+require (
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+//	"io/ioutil"
+	"log"
+	"golang.org/x/crypto/ssh"
+	"time"
+)
+
+// SSHConfig stores SSH connection information
+type SSHConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+}
+
+func main() {
+	config := SSHConfig{
+		Host:     "192.168.1.1",  // Replace with your router's IP
+		Port:     "22",           // SSH port
+		Username: "root",        // Replace with your SSH username
+		Password: "ABCabc12",     // Replace with your SSH password
+	}
+
+	// Create SSH client configuration
+	sshConfig := &ssh.ClientConfig{
+		User: config.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(config.Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // Not secure, for testing purposes only
+		Timeout:         5 * time.Second,
+	}
+
+	// Connect to the router
+	address := fmt.Sprintf("%s:%s", config.Host, config.Port)
+	client, err := ssh.Dial("tcp", address, sshConfig)
+	if err != nil {
+		log.Fatalf("Failed to dial: %s", err)
+	}
+	defer client.Close()
+
+	// Open a session
+	
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create session: %s", err)
+	}
+	defer session.Close()
+	// Run a command on the router
+	output, err := session.CombinedOutput("cat /etc/openwrt_release") // Replace with the command you want to run
+	if err != nil {
+		log.Fatalf("Failed to run command: %s", err)
+	}
+
+	// Print the output of the command
+	fmt.Println(string(output))
+}
